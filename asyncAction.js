026041884b4dfe9f99af4f71971b726383c5db1c
Aashiish-Pandey <ash006pandey@gmail.com>
2021-04-11 11:51:31 +0530

    using redux thunk middleware

--- asyncAction.js
+++ asyncAction.js
@@ -1,5 +1,8 @@
-const redux = require('redux')
+const redux = require('redux');
 const createStore= redux.createStore
+const applyMiddleware = redux.applyMiddleware
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
 
 const intialState = {
   loading: false,
@@ -19,14 +22,14 @@ const fetchUsersRequest = () => {
   };
 };
 
-const FETCH_USERS_SUCCESS = (users) => {
+const fetchUsersSuccess = (users) => {
   return {
     type: FETCH_USERS_SUCCESS,
     payload: users,
   };
 };
 
-const FETCH_USERS_FAILURE = (errors) => {
+const fetchUsersFailure = (errors) => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error,
@@ -60,7 +63,31 @@ const reducer = (state = intialState, action) => {
   }
 };
 
+const fetchUsers = ()=> {
+
+    return function(dispatch) {
+
+        dispatch(fetchUsersRequest())
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response =>{
+
+            const users = response.data.map(user=>user.id)
+            dispatch(fetchUsersSuccess(users))
+
+        })
+        .catch(error=>{
+
+            dispatch(fetchUsersFailure(error.message))
+
+
+        })
+    }
+}
+
 
 // Creation of Store:
 
-const store = createStore(reducer)
+const store = createStore(reducer,applyMiddleware(thunkMiddleware))
+
+store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(fetchUsers)
